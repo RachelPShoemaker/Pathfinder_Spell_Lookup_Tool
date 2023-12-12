c2fabf9b800b166a7bac5c91db31a5f3f3190d25
Rachel Shoemaker <rachelpshoemaker@gmail.com>
2023-12-11 19:54:16 -0600

    Add files via upload

---
+++ spell.py
@@ -0,0 +1,37 @@
+class Spell:
+    def __init__(self):
+        self.name = " "
+        self.actions = " "
+        self.traits = []
+        self.school = " "
+        self.traditions = []
+        self.cast = []
+        self.deities = []
+        self.requirement = " "
+        self.trigger = " "
+        self.cost = " "
+        self.range = " "
+        self.target = " "
+        self.area = " "
+        self.duration = " "
+        self.save_throw = " "
+        self.description = " "
+    
+    def print_spell(self):
+        print('Spell name: ',self.name)
+        print(self.actions)
+        if(self.school != " "): print('School: ',self.school)
+        if(self.traits != []): print('Traits:',', '.join(self.traits))
+        if(self.traditions != []): print('Tradition(s): ',', '.join(self.traditions))
+        if(self.cast != []): print('Cast type:',', '.join(self.cast))
+        if(self.deities != []): print('Deities:',', '.join(self.deities))
+        if(self.requirement != " "): print('Requirements:',self.requirement)
+        if(self.trigger != " "): print('Trigger:',self.trigger)
+        if(self.cost != " "): print('Cost:',self.cost)
+        if(self.range != " "): print('Range:',self.range)
+        if(self.target != " "): print('Target:',self.target)
+        if(self.area != " "): print('Area:',self.area)
+        if(self.duration != " "): print('Duration:',self.duration)
+        if(self.save_throw != " "): print('Saving throw:',self.save_throw)
+        print('\n',self.description)
+        print("------------------------------------")
